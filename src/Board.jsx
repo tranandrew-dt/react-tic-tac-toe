07d43b5dfaa5b0eb5feb135b99ec21a7b3950adc
Andrew D Tran <tranandrew.dt@gmail.com>
2024-08-24 18:04:21 -0700

    [feat] added new Component called Board.jsx, which will handle the board itself and will be fed to App.jsx

---
+++ src/Board.jsx
@@ -0,0 +1,9 @@
+//app just renders board
+//board needs state of all 9 squares, whether x or o
+//state of player turn (or whether x or o is next
+//maybe a map to render all three rows instead of hardcoding lmao
+//how is a winner determined? Is there a way to search the combinations of the squares??
+// hope i don't have to hard-code win conditions...
+// I mean i guess it's just all hori, verti, and the two diags (3+3+2)
+// If i know who is next, can't i show an onHover of their icon on empty squares?
+//...shit also look fugly atm
