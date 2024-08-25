a79950f392c5584c22cf847b7e9e7bada9743d09
Andrew D Tran <tranandrew.dt@gmail.com>
2024-08-24 18:03:41 -0700

    [feat] added an onclick effect with state tracking for the Square component, it just shows or hides on click for now

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
+//If state of square (from board) is occupied, don't allow overwrite
+
 function Square(props) {
   const [clickStatus, setClickStatus] = useState(false);
 
-  function handleClickStatus() {}
+  function handleClickStatus() {
+    setClickStatus((prev) => {
+      return !prev;
+    });
+  }
 
   return (
     <button className="square" onClick={handleClickStatus}>
-      {props.num}
+      {clickStatus && props.num}
     </button>
   );
 }
