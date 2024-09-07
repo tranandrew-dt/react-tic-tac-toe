853dbbb93ccd2c8c459a390f9f34ed51b19b57c9
Andrew D Tran <tranandrew.dt@gmail.com>
2024-09-07 15:33:55 -0700

    [fix] fixed bug where checkWinners was checking state, also fixed bug where the return null was inside the loop, so only the first line's win condition ever triggered

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -30,6 +30,7 @@ function Board() {
     setBoard((prev) => {
       const newBoardState = [...prev];
       newBoardState[id] = player;
+      calculateWinner(newBoardState);
       return newBoardState;
     });
   }
@@ -40,7 +41,8 @@ function Board() {
     });
   }
 
-  function calculateWinner(squares) {
+  function calculateWinner(board) {
+    console.log(board);
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -51,17 +53,13 @@ function Board() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let i = 0; i < lines.size(); i++) {
+    for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
       }
-      return null;
     }
+    return null;
   }
 
   //TODO fix later with complex states
